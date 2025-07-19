246e3bb41093c26f2f2b8d8b9a5c5258993100db
Jeffrey Wang <jeffwang125@gmail.com>
2025-07-19 19:18:29 -0400

    just one install script in pub (merge environment and image)

---
+++ pub/setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e -o pipefail
+
+instance_name="ITSC-3146"
+repo_url="https://raw.githubusercontent.com/jeffreyalanwang/ITSC_3146_Lima/refs/heads/main"
+
+command_available() {
+    /usr/bin/env which -s $1
+}
+
+environment_setup() {
+    export NONINTERACTIVE=1
+
+    # Install homebrew, if not present
+    if ! (command_available "brew"); then
+        echo "Installing homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Homebrew found at $(which "brew")."
+    fi
+
+    # Install xquartz, if not present
+    if ! (command_available "xquartz"); then
+    echo "Installing xquartz..."
+        brew install --cask xquartz
+    else
+        echo "XQuartz found at $(which "xquartz")."
+    fi
+
+    # Install lima, if not present
+    if ! (command_available "lima"); then
+        echo "Installing lima..."
+        brew install lima
+    else
+        echo "Lima found at $(which "lima")."
+    fi
+
+    # Install vscode, if not present
+    if ! (command_available "code") && ! (ls "/Applications/" | grep -q "Visual Studio Code"); then
+        echo "Installing Visual Studio Code..."
+        brew install --cask visual-studio-code
+    else
+        echo "Visual Studio Code found at $(which "code" || echo "/Applications/")."
+    fi
+
+    echo "Completed successfully."
+}
+
+install_instance() {
+    # create image
+    echo "Create Lima instance ${instance_name}..."
+    limactl create --tty=false "${repo_url}/pub/${instance_name}.yaml"
+
+    # configure host SSH
+    echo "Adding instance SSH config to ~/.ssh/config..."
+    echo '' >> ~/.ssh/config # newline
+    echo '# Lima instances' >> ~/.ssh/config
+    echo "# (this line created by ${instance_name} setup.sh)" >> ~/.ssh/config
+    echo 'Include ~/.lima/*/ssh.config' >> ~/.ssh/config
+
+    # configure macOS Terminal
+    echo "Adding instance as a profile in Terminal app..."
+    curl "${repo_url}/pub/profile.terminal" > "~/Downloads/${instance_name}.terminal"
+    open "~/Downloads/${instance_name}.terminal"
+}
+
+environment_setup
+install_instance
